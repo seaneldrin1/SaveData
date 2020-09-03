804f85f03c3d5959bd9572365d43c4814d53e6c2
sean.onyo <seantell.onyo12@gmail.com>
2020-09-03 13:34:47 +0300

    adding first Attempt

---
+++ app/src/main/java/com/example/savedata/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.savedata;
+
+import android.app.Person;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity  {
+    EditText etName, etSurname;
+    TextView tvResult;
+    ArrayList<Person> persons;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etName = findViewById(R.id.etName);
+        etSurname= findViewById(R.id.etSurname);
+        tvResult= findViewById(R.id.tvResult);
+        persons = new ArrayList<Person>();
+
+
+    }
+    public void btnAddDta(View v){
+
+    }
+    public void btnSaveData(View v){
+
+    }
+}
